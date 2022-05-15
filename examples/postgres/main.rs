d87f3aaaa2907d04da6c81f7f302558ed43893b7
Saurav Sharma <appdroiddeveloper@gmail.com>
2022-05-15 09:20:56 +0545

    chore(examples): add examples of both direct function and cli
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- examples/postgres/main.rs
+++ examples/postgres/main.rs
@@ -8,7 +8,11 @@ async fn main() {
     let pool = sqlx::Pool::connect(&uri).await.unwrap();
     let mut migrator = Migrator::new_from_pool(&pool);
     migrator.add_migrations(migrations::migrations());
+    // There are two way to run migration. Either you can create cli as shown below
     sqlx_migrator::clap::run_cli(Box::new(migrator))
         .await
         .unwrap();
+    // Or you can run migrator apply function as required as commented out part
+    // below
+    // migrator.apply_all().await.unwrap();
 }
