4d6f1236518a71fa1e1c8e43f4ab06756aa1dbe9
Saurav Sharma <appdroiddeveloper@gmail.com>
2022-07-25 15:50:42 +0545

    chore(examples): update postgres database url env
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- examples/postgres/main.rs
+++ examples/postgres/main.rs
@@ -4,7 +4,7 @@ use sqlx_migrator::postgres::migrator::Migrator;
 mod migrations;
 #[tokio::main]
 async fn main() {
-    let uri = std::env::var("SQLITE_DATABASE_URL").unwrap();
+    let uri = std::env::var("POSTGRES_DATABASE_URL").unwrap();
     let pool = sqlx::Pool::connect(&uri).await.unwrap();
     let mut migrator = Migrator::new(&pool);
     migrator.add_migrations(migrations::migrations());
