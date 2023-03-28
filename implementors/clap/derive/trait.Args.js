f2ded5ddb95a6a0fdc68eb909d0ec5124dea12c4
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-03-28 03:04:13 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@739d2989ffbaf988ce95883fc2a21198e63bc5a3 🚀

--- implementors/clap/derive/trait.Args.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"sqlx_migrator":[["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Apply.html\" title=\"struct sqlx_migrator::cli::Apply\">Apply</a>"],["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Revert.html\" title=\"struct sqlx_migrator::cli::Revert\">Revert</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sqlx_migrator":[["impl CommandFactory for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Apply.html\" title=\"struct sqlx_migrator::cli::Apply\">Apply</a>"],["impl CommandFactory for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Revert.html\" title=\"struct sqlx_migrator::cli::Revert\">Revert</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
