b208f29232162d793e161ca77eddfb6c4fd2b0d8
Saurav Sharma <appdroiddeveloper@gmail.com>
2023-05-12 16:05:13 +0545

    docs: change location of custom migrator example
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@
 #![warn(clippy::pedantic)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
-//! Library to create sqlx migration using rust code instead of sql
+//! Library to create sqlx migration using rust code instead of sql. Visit
+//! examples and readme file to learn of how to use sqlx migrator on sqlx
+//! project
+
 #[cfg(feature = "cli")]
 pub mod cli;
 pub mod error;
