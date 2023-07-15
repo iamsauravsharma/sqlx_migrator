0c82333e296ec1b9de7aba15922cff85fc67557c
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-07-15 16:24:19 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@b2540b8806e46145a06cf8a2e5e20392ac9ca07f 🚀

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sqlx_migrator":[["impl&lt;DB&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"sqlx_migrator/migrator/struct.Migrator.html\" title=\"struct sqlx_migrator::migrator::Migrator\">Migrator</a>&lt;DB&gt;<span class=\"where fmt-newline\">where\n    DB: Database,</span>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
