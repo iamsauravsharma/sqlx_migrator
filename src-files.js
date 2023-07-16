f39871651d88260b43c6afce4f0b0a8e54fc67a0
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-07-16 01:41:49 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@23dbdaad11b14e64baa1ab47cb00e52723fd15ab 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var sourcesIndex = JSON.parse('{\
+var srcIndex = JSON.parse('{\
 "sqlx_migrator":["",[["migrator",[],["any.rs","mod.rs","mysql.rs","postgres.rs","sqlite.rs"]]],["cli.rs","error.rs","lib.rs","migration.rs","operation.rs"]]\
 }');
-createSourceSidebar();
+createSrcSidebar();
