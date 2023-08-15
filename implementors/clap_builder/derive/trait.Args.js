2b3eeefc6056633de3ce41d2823fe6acb5d19843
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-08-15 02:33:05 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@89b577b406539bc344404dc052aa6c991c0e6da9 🚀

--- implementors/clap_builder/derive/trait.Args.js
+++ implementors/clap_builder/derive/trait.Args.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"sqlx_migrator":[["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Apply.html\" title=\"struct sqlx_migrator::cli::Apply\">Apply</a>"],["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Revert.html\" title=\"struct sqlx_migrator::cli::Revert\">Revert</a>"]]
+"sqlx_migrator":[["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Revert.html\" title=\"struct sqlx_migrator::cli::Revert\">Revert</a>"],["impl Args for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Apply.html\" title=\"struct sqlx_migrator::cli::Apply\">Apply</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
