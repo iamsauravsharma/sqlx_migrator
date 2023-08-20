ff252982aa7f2b87977653c48e31554cdd2c428f
Saurav Sharma <appdroiddeveloper@gmail.com>
2023-08-20 08:59:18 +0545

    chore(deps): only support serde upto 1.0.171
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![warn(missing_docs, unreachable_pub)]
+#![warn(missing_docs, unreachable_pub, unused_crate_dependencies)]
 #![deny(unsafe_code)]
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
@@ -7,6 +7,9 @@
 //! Library to create sqlx migration using rust code instead of sql. Visit
 //! examples and readme file to learn of how to use sqlx migrator on sqlx
 //! project
+
+// use serde to handle unused_crate_dependencies issue for serde
+use serde as _;
 
 #[cfg(feature = "cli")]
 pub mod cli;
