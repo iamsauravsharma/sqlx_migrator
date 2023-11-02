e1794d24ae4b7b33db5bcbc3514a110c7c8c3c8f
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-11-02 15:42:00 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@e4dc357e69111246cc1e09bbfdac6e837c54b0f6 🚀

--- implementors/clap_builder/derive/trait.Parser.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"sqlx_migrator":[["impl Parser for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Apply.html\" title=\"struct sqlx_migrator::cli::Apply\">Apply</a>"],["impl Parser for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.Revert.html\" title=\"struct sqlx_migrator::cli::Revert\">Revert</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
