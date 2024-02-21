2aacf5c79c60c69d0b19d635d81b3e9e6a99afff
Saurav Sharma <appdroiddeveloper@gmail.com>
2024-02-21 17:03:47 +0545

    feat: support expr instead of only supporting literal for some macros
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/macros.rs
+++ src/macros.rs
@@ -19,12 +19,12 @@ macro_rules! vec_box {
 ///   (backward migration).
 #[macro_export]
 macro_rules! operation {
-    ($db:ty, $op:ty, $up:literal, $down:literal) => {
+    ($db:ty, $op:ty, $up:expr, $down:expr) => {
         #[async_trait::async_trait]
         impl sqlx_migrator::operation::Operation<$db> for $op {
             async fn up(
                 &self,
-                connection: &mut <$db as sqlx::Database>::Connection,
+                connection: &mut <$db as sqlx_migrator::sqlx::Database>::Connection,
             ) -> Result<(), sqlx_migrator::error::Error> {
                 sqlx_migrator::sqlx::query($up).execute(connection).await?;
                 Ok(())
@@ -32,7 +32,7 @@ macro_rules! operation {
 
             async fn down(
                 &self,
-                connection: &mut <$db as sqlx::Database>::Connection,
+                connection: &mut <$db as sqlx_migrator::sqlx::Database>::Connection,
             ) -> Result<(), sqlx_migrator::error::Error> {
                 sqlx_migrator::sqlx::query($down)
                     .execute(connection)
@@ -53,7 +53,7 @@ macro_rules! operation {
     feature = "any"
 ))]
 macro_rules! any_operation {
-    ($op:ty, $up:literal, $down:literal) => {
+    ($op:ty, $up:expr, $down:expr) => {
         sqlx_migrator::operation!(sqlx_migrator::sqlx::Any, $op, $up, $down);
     };
 }
@@ -65,7 +65,7 @@ macro_rules! any_operation {
 #[macro_export]
 #[cfg(feature = "mysql")]
 macro_rules! mysql_operation {
-    ($op:ty, $up:literal, $down:literal) => {
+    ($op:ty, $up:expr, $down:expr) => {
         sqlx_migrator::operation!(sqlx_migrator::sqlx::MySql, $op, $up, $down);
     };
 }
@@ -77,7 +77,7 @@ macro_rules! mysql_operation {
 #[macro_export]
 #[cfg(feature = "postgres")]
 macro_rules! postgres_operation {
-    ($op:ty, $up:literal, $down:literal) => {
+    ($op:ty, $up:expr, $down:expr) => {
         sqlx_migrator::operation!(sqlx_migrator::sqlx::Postgres, $op, $up, $down);
     };
 }
@@ -89,7 +89,7 @@ macro_rules! postgres_operation {
 #[macro_export]
 #[cfg(feature = "sqlite")]
 macro_rules! sqlite_operation {
-    ($op:ty, $up:literal, $down:literal) => {
+    ($op:ty, $up:expr, $down:expr) => {
         sqlx_migrator::operation!(sqlx_migrator::sqlx::Sqlite, $op, $up, $down);
     };
 }
@@ -102,13 +102,13 @@ macro_rules! sqlite_operation {
 ///
 /// This macro expects the following arguments:
 /// - `$db:ty`: the type of database
-/// - `$app_name:literal`: Name of app to be used for app variable
+/// - `$app_name:expr`: Name of app to be used for app variable
 /// - `$op:ty`: The type for which the migration is being implemented
 /// - `$parents:expr`: List of parents migration.
 /// - `$operations:expr`: List of operations
 #[macro_export]
 macro_rules! migration {
-    ($db:ty, $app_name:literal, $op:ty, $parents:expr, $operations:expr) => {
+    ($db:ty, $app_name:expr, $op:ty, $parents:expr, $operations:expr) => {
         #[async_trait::async_trait]
         impl sqlx_migrator::migration::Migration<$db> for $op {
             fn app(&self) -> &str {
@@ -143,7 +143,7 @@ macro_rules! migration {
     feature = "any"
 ))]
 macro_rules! any_migration {
-    ($app_name:literal, $op:ty, $parents:expr, $operations:expr) => {
+    ($app_name:expr, $op:ty, $parents:expr, $operations:expr) => {
         sqlx_migrator::migration!(
             sqlx_migrator::sqlx::Any,
             $app_name,
@@ -161,7 +161,7 @@ macro_rules! any_migration {
 #[macro_export]
 #[cfg(feature = "mysql")]
 macro_rules! mysql_migration {
-    ($app_name:literal, $op:ty, $parents:expr, $operations:expr) => {
+    ($app_name:expr, $op:ty, $parents:expr, $operations:expr) => {
         sqlx_migrator::migration!(
             sqlx_migrator::sqlx::MySql,
             $app_name,
@@ -179,7 +179,7 @@ macro_rules! mysql_migration {
 #[macro_export]
 #[cfg(feature = "postgres")]
 macro_rules! postgres_migration {
-    ($app_name:literal, $op:ty, $parents:expr, $operations:expr) => {
+    ($app_name:expr, $op:ty, $parents:expr, $operations:expr) => {
         sqlx_migrator::migration!(
             sqlx_migrator::sqlx::Postgres,
             $app_name,
@@ -197,7 +197,7 @@ macro_rules! postgres_migration {
 #[macro_export]
 #[cfg(feature = "sqlite")]
 macro_rules! sqlite_migration {
-    ($app_name:literal, $op:ty, $parents:expr, $operations:expr) => {
+    ($app_name:expr, $op:ty, $parents:expr, $operations:expr) => {
         sqlx_migrator::migration!(
             sqlx_migrator::sqlx::Sqlite,
             $app_name,
