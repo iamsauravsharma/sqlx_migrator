730800a7ee9e721e6f258e75d89d13d56ac7c6df
iamsauravsharma <appdroiddeveloper@gmail.com>
2024-02-21 10:21:55 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@f3fd12d53c804b425c8a1fe6ce242dd92cc86eeb 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = new Map(JSON.parse('[\
-["sqlx_migrator",["",[["migrator",[],["any.rs","mod.rs","mysql.rs","postgres.rs","sqlite.rs"]]],["cli.rs","error.rs","lib.rs","migration.rs","operation.rs"]]]\
+["sqlx_migrator",["",[["migrator",[],["any.rs","mod.rs","mysql.rs","postgres.rs","sqlite.rs"]]],["cli.rs","error.rs","lib.rs","macros.rs","migration.rs","operation.rs"]]]\
 ]'));
 createSrcSidebar();
