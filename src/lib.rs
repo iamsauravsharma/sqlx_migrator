8ed53c236e0cbf7c15f7ecddb4dbb0da3abd2435
Saurav Sharma <appdroiddeveloper@gmail.com>
2024-10-12 13:00:24 +0545

    refactor: reexport some common struct and trait
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,15 @@
 
 //! Library to create sqlx migration using rust code instead of sql.
 //!
-//! Visit examples directory and readme file to learn of how to use sqlx
-//! migrator on sqlx project
+//! Check `README.MD` for more detailed information of how to use a crate
+//! and visit [`Operation`], [`Migration`] and [`Migrator`]
+
+#[cfg(feature = "cli")]
+pub use crate::cli::MigrationCommand;
+pub use crate::error::Error;
+pub use crate::migration::Migration;
+pub use crate::migrator::{Info, Migrate, Migrator, Plan};
+pub use crate::operation::Operation;
 
 #[cfg(feature = "cli")]
 pub mod cli;
