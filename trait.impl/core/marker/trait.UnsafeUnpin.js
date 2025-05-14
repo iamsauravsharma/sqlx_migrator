d1c962aa70b0dc55a34c5c03919a29fd6df09551
iamsauravsharma <appdroiddeveloper@gmail.com>
2025-05-14 12:54:43 +0000

    Deploying to gh-pages from @ iamsauravsharma/sqlx_migrator@92ab699da5cde24486f475c57e1503f7b51e8bbb 🚀

---
+++ trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sqlx_migrator",[["impl UnsafeUnpin for <a class=\"enum\" href=\"sqlx_migrator/error/enum.Error.html\" title=\"enum sqlx_migrator::error::Error\">Error</a>",1,["sqlx_migrator::error::Error"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"sqlx_migrator/cli/struct.MigrationCommand.html\" title=\"struct sqlx_migrator::cli::MigrationCommand\">MigrationCommand</a>",1,["sqlx_migrator::cli::MigrationCommand"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"sqlx_migrator/migration/struct.AppliedMigrationSqlRow.html\" title=\"struct sqlx_migrator::migration::AppliedMigrationSqlRow\">AppliedMigrationSqlRow</a>",1,["sqlx_migrator::migration::AppliedMigrationSqlRow"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"sqlx_migrator/migrator/struct.Plan.html\" title=\"struct sqlx_migrator::migrator::Plan\">Plan</a>",1,["sqlx_migrator::migrator::Plan"]],["impl&lt;DB&gt; UnsafeUnpin for <a class=\"struct\" href=\"sqlx_migrator/migrator/struct.Migrator.html\" title=\"struct sqlx_migrator::migrator::Migrator\">Migrator</a>&lt;DB&gt;",1,["sqlx_migrator::migrator::Migrator"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1082]}
